b4722bba47433d26bb795135977e1cf4ada60f04
abuh1 <abuhamza2308@hotmail.com>
2023-02-15 17:45:24 +0000

    Update

--- blackjack.py
+++ blackjack.py
@@ -17,14 +17,19 @@ If your hand is worth 17 points but the dealer only has a 5 and an 8 (total = 13
 Each round, you will be able to hit (draw another card) or stand (keep your current cards and don't draw). 
 """
 
+STARTING_CASH = 1000
 # Create a list of all unique cards
 unique_cards = ['A', 'J', 'Q', 'K']
 for i in range(2,11):
     unique_cards.insert(i-1, str(i))
+# Points for each card in a dict
+points = {k:k for k in unique_cards}
+# Change A, J, Q, K value in dict. A will be 11 unless total is over 21, then it will change to 1.
+points['A'] = '11'
+points['J'], points['Q'], points['K'] = '10', '10', '10'
     
 def main():
     print("TYPE CTRL+C IF YOU WAN'T TO STOP THE PROGRAM AT ANY TIME\n" + "-" * 105)
-    cash = 1000
     # Asks user if they want to read the rules
     while True:
         print("Welcome to Blackjack! Do you want to read the rules? (y/n)")
@@ -39,16 +44,20 @@ def main():
         print('-' * 105)
     # Round loop
     while True:
+        cash = STARTING_CASH
         print("Your Balance:", cash)
-        bet = place_bet(cash=cash)
-    
+        bet = place_bet(cash)
+        cash -= bet
         deck = Deck()
         dealer_hand, player_hand = [], []
         # Initial deal - 2 cards each
         deck.deal(player_hand, dealer_hand)
         dealer_hidden = [dealer_hand[0], "?"]
-        round(player_hand, dealer_hand, dealer_hidden, cash)
-                
+        dealer_hand[0] = 'A'
+        dealer_hand[1] = '10'
+        
+        round(player_hand, dealer_hand, dealer_hidden, bet)
+        
 class Deck():
     def __init__(self):
         self.deck = []
@@ -77,27 +86,48 @@ class Deck():
             count += 1
 
 # Function to place bets
-def place_bet(cash):
+def place_bet(balance):
     while True:
         print("Please place your bet: (2.00 to 500.0)")
         bet = input(">")
         try:
             bet = int(bet)
-            if bet > cash:
-                print(f"You do not have enough money. Your current balance is: {cash}\n")
+            if bet > balance:
+                print(f"You do not have enough money. Your current balance is: {balance}\n")
             elif bet < 2 or bet > 500:
                 print("You must enter a value between 2 and 500.\n")
             else:
                 break
         except ValueError:
             print("Please enter an integer.\n")
+    return bet
             
-# This will loop per round. Takes player, dealer, and hidden dealer hands and cash.
-def round(player, dealer, dealer_h, cash):
+# This will loop per round
+def round(player, dealer, dealer_hidden, bet):
     print("-" * 105)
-    print(f"DEALER HAND: {dealer_h}\n\n")
+    print(f"DEALER HAND: {dealer_hidden}\n\n")
     print(f"YOUR HAND: {player}\n")
     print("-" * 105)
+    # Check naturals
+    dealer_bj = False
+    player_bj = False
+    if int(points[player[0]]) + int(points[player[1]]) == 21:
+        player_bj = True
+    if int(points[dealer[0]]) + int(points[dealer[1]]) == 21:
+        dealer_bj = True
+    if player_bj and dealer_bj:
+        print("The dealer and the player both have blackjack! Returning bets...")
+        print("-" * 105)
+        cash += bet
+    elif player_bj and not dealer_bj:
+        print("The player has a blackjack! 1.5x return...")
+        print("-" * 105)
+        cash += 1.5*bet
+    elif dealer_bj and not player_bj:
+        print("The dealer has a blackjack! Sorry, you lose this round...")
+        print("-" * 105)
+        
+    
         
 if __name__ == '__main__':
     main()
