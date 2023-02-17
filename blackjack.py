b128668bab1e6cab5880d10cb6951d1961b153dd
abuh1 <abuhamza2308@hotmail.com>
2023-02-17 15:46:32 +0000

    Update

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,18 @@
 from itertools import repeat
 import random
 
+# Initial starting money
+balance = 1000
+# Create a list of all unique cards
+unique_cards = ['A', 'J', 'Q', 'K']
+for i in range(2,11):
+    unique_cards.insert(i-1, str(i))
+# Points for each card in a dict
+points = {k:k for k in unique_cards}
+# Change A, J, Q, K value in dict. A will be 11 unless total is over 21, then it will change to 1.
+points['A'] = '11'
+points['J'], points['Q'], points['K'] = '10', '10', '10'
+
 # Rules of blackjack
 rules = """Here are the rules:
 The dealer will deal two cards to each player, including the dealer himself. You will be able to see both
@@ -17,18 +29,43 @@ If your hand is worth 17 points but the dealer only has a 5 and an 8 (total = 13
 Each round, you will be able to hit (draw another card) or stand (keep your current cards and don't draw). 
 """
 
-STARTING_CASH = 1000
-# Create a list of all unique cards
-unique_cards = ['A', 'J', 'Q', 'K']
-for i in range(2,11):
-    unique_cards.insert(i-1, str(i))
-# Points for each card in a dict
-points = {k:k for k in unique_cards}
-# Change A, J, Q, K value in dict. A will be 11 unless total is over 21, then it will change to 1.
-points['A'] = '11'
-points['J'], points['Q'], points['K'] = '10', '10', '10'
-    
+# Deck class
+class Deck():
+    def __init__(self):
+        self.deck = []
+        self.burned = []
+        for u in unique_cards:
+            self.deck.extend(repeat(u, 4))
+    def __repr__(self):
+        return str(self.deck)
+    # Initial shuffle
+    def shuffle_burn(self):
+        # Shuffle and burn top card
+        random.shuffle(self.deck)
+        burn = self.deck.pop()
+        self.burned.append(burn)
+    # Draw card into dealer or player's hand, and appends it to burned to keep all removed cards for reshuffling later
+    def draw(self, hand):
+        top_card = self.deck.pop()
+        hand.append(top_card)
+        self.burned.append(top_card)
+    # Initial deal
+    def deal(self, *args: list):
+        # Reshuffle the deck when it gets down to half
+        if len(self.deck) <= 26:
+            for i in self.burned:
+                self.deck.append(i)
+            self.burned = []
+            self.shuffle_burn()
+        # Deals two cards to each player
+        count = 0
+        while count < 2:
+            for p in args:
+                self.draw(p)
+            count += 1
+
 def main():
+    global balance
     print("TYPE CTRL+C IF YOU WAN'T TO STOP THE PROGRAM AT ANY TIME\n" + "-" * 105)
     # Asks user if they want to read the rules
     while True:
@@ -44,47 +81,61 @@ def main():
         print('-' * 105)
     # Round loop
     while True:
-        cash = STARTING_CASH
-        print("Your Balance:", cash)
-        bet = place_bet(cash)
-        cash -= bet
+        print("Your Balance:", balance)
+        bet = place_bet(balance)
+        balance -= bet
+        # Creates deck and shuffles it
         deck = Deck()
+        deck.shuffle_burn()
+        
         dealer_hand, player_hand = [], []
         # Initial deal - 2 cards each
         deck.deal(player_hand, dealer_hand)
         dealer_hidden = [dealer_hand[0], "?"]
-        dealer_hand[0] = 'A'
-        dealer_hand[1] = '10'
         
-        round(player_hand, dealer_hand, dealer_hidden, bet)
+        print("-" * 105)
+        print(f"DEALER HAND: {dealer_hidden}\n\n")
+        print(f"YOUR HAND: {player_hand}\n")
+        print("-" * 105)
         
-class Deck():
-    def __init__(self):
-        self.deck = []
-        self.burned = []
-        for u in unique_cards:
-            self.deck.extend(repeat(u, 4))
-    def __repr__(self):
-        return str(self.deck)
-    # Initial shuffle
-    def shuffle_burn(self):
-        # Shuffle and burn top card
-        random.shuffle(self.deck)
-        burn = self.deck.pop()
-        self.burned.append(burn)
-    # Draw card into dealer or player's hand
-    def draw(self, hand):
-        hand.append(self.deck.pop())
-    # Initial deal
-    def deal(self, *args: list):
-        self.shuffle_burn()
-        # Deals two cards to each player
-        count = 0
-        while count < 2:
-            for p in args:
-                self.draw(p)
-            count += 1
-
+        naturals(player_hand, dealer_hand, bet)
+        round(deck, player_hand, dealer_hand, bet)
+        
+# Rest of the round after checking naturals
+def round(deck, player, dealer, bet):
+    while True:
+        move = input("Type 1 to hit, or 2 to stand.")
+        try:
+            move = int(move)
+            if move != 1 and move != 2:
+                print("Error: Type 1 or 2")
+            else:
+                break
+        except ValueError:
+            print("Error: Type 1 or 2")
+        
+# Checks if player or dealer have blackjack
+def naturals(player, dealer, bet):
+    global balance
+    dealer_bj = False
+    player_bj = False
+    
+    if int(points[player[0]]) + int(points[player[1]]) == 21:
+        player_bj = True
+    if int(points[dealer[0]]) + int(points[dealer[1]]) == 21:
+        dealer_bj = True
+    if player_bj and dealer_bj:
+        print("The dealer and the player both have blackjack! Returning bets...")
+        print("-" * 105)
+        balance += bet
+    elif player_bj and not dealer_bj:
+        print("The player has a blackjack! 1.5x return...")
+        print("-" * 105)
+        balance += 1.5*bet
+    elif dealer_bj and not player_bj:
+        print("The dealer has a blackjack! Sorry, you lose this round...")
+        print("-" * 105)
+          
 # Function to place bets
 def place_bet(balance):
     while True:
@@ -101,33 +152,6 @@ def place_bet(balance):
         except ValueError:
             print("Please enter an integer.\n")
     return bet
-            
-# This will loop per round
-def round(player, dealer, dealer_hidden, bet):
-    print("-" * 105)
-    print(f"DEALER HAND: {dealer_hidden}\n\n")
-    print(f"YOUR HAND: {player}\n")
-    print("-" * 105)
-    # Check naturals
-    dealer_bj = False
-    player_bj = False
-    if int(points[player[0]]) + int(points[player[1]]) == 21:
-        player_bj = True
-    if int(points[dealer[0]]) + int(points[dealer[1]]) == 21:
-        dealer_bj = True
-    if player_bj and dealer_bj:
-        print("The dealer and the player both have blackjack! Returning bets...")
-        print("-" * 105)
-        cash += bet
-    elif player_bj and not dealer_bj:
-        print("The player has a blackjack! 1.5x return...")
-        print("-" * 105)
-        cash += 1.5*bet
-    elif dealer_bj and not player_bj:
-        print("The dealer has a blackjack! Sorry, you lose this round...")
-        print("-" * 105)
-        
-    
         
 if __name__ == '__main__':
     main()
